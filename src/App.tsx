1f740eaa4ab57daf7e190977166a3951f79a1919
vickp <53214077+vickp@users.noreply.github.com>
2021-07-31 16:47:21 +0900

    Update: Useless file deleted. Product info updated.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ import icon from '../assets/icon.svg';
 import './App.global.css';
 
 const Hello = () => {
+
+  // convertBtn.addEventListener("click", (e) => {
+  //   convertAll.innerText = `.\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${
+  //     url.value
+  //   }" -c copy -bsf:a aac_adtstoasc "${url.value.substring(-8)}.mp4"`;
+
+  //   convertHour.innerText = `.\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${
+  //     url.value
+  //   }" -c copy -flags +global_header -f segment -segment_time 3600 -segment_format_options movflags=+faststart -reset_timestamps 1  "${url.value.substring(
+  //     -8
+  //   )}_60_%d.mp4"`;
+  // });
+
   return (
     <>
      {/* <div>
