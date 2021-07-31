50f94833289eb7cd610b47b6c55c03af036c381c
vickp <53214077+vickp@users.noreply.github.com>
2021-07-31 17:52:23 +0900

    Update: Segment option changed.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   const generateAddr = (addr) => {
       console.log(`.\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${addr}" -c copy -bsf:a aac_adtstoasc "${addr.substring(-8)}.mp4"`);
-      setAddr(`.\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${addr}" -c copy -bsf:a aac_adtstoasc "${addr.substring(-8)}.mp4"`);
+      setAddr(`.\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${addr}" -c copy -flags +global_header -f segment -segment_time 3600 -segment_format_options movflags=+faststart -reset_timestamps 1 "${addr.substring(-8)}.mp4"`);
   }
 
   const handleChange = (e) => {
