cca08d0d4a0b7e72c22c9ead6954dcb1ccc90527
vickp <53214077+vickp@users.noreply.github.com>
2023-02-05 15:37:10 +0900

    Update: date auto generate added.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Typography } from '@material-ui/core';
 import { ipcRenderer } from 'electron';
 
-
 const Home = () => {
   // convertBtn.addEventListener("click", (e) => {
   //   convertAll.innerText = `.\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${
@@ -21,40 +20,74 @@ const Home = () => {
   });
   const [addr, setAddr] = useState('');
 
-  const generateAddr = (addr) => {
-    console.log(addr);
+  const generateAddr = (addr: string) => {
+    const date = new Date();
 
-    const command = `\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${addr}" -c copy -bsf:a aac_adtstoasc "${addr.substring(-8)}.mp4"`;
-    setAddr(`\\.ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${addr}" -c copy -flags +global_header -f segment -segment_time 3600 -segment_format_options movflags=+faststart -reset_timestamps 1 "${addr.substring(-8)}.mp4"`);
+    const day = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds()}`;
+    setAddr(
+      `.\\\\ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "${addr}" -c copy -flags +global_header -f segment -segment_time 3600 -segment_format_options movflags=+faststart -reset_timestamps 1 "${day}_%d.mp4"`
+    );
+  };
 
-    executeCollect(command);
-  }
-
-  const handleChange = (e) => {
+  const handleChange = (e: { target: { value: any } }) => {
     setValues({
-      address: e.target.value
-    })
-  }
-
-  const executeCollect = (args) => {
-    // ipcRenderer.send('execute', args);
-  }
+      address: e.target.value,
+    });
+  };
 
   return (
-    <>
-      <Typography style={{textAlign: 'center'}} variant="h4">FFmpeg Generator - Converting M3U8 to Command</Typography>
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', marginTop: '64px'}}>
-        <input type="text" onChange={handleChange} style={{ width: '306px', height: '28px' }}/>
-        <button type="button" onClick={() => {
-          generateAddr(values?.address);
-        }} style={{ width: '96px', height: '48px', marginLeft: '12px' }}>변환</button>
+    <div
+      style={{
+        width: 'inherit',
+        height: 'inherit',
+        overflow: 'hidden',
+      }}
+    >
+      <Typography style={{ textAlign: 'center' }} variant="h4">
+        FFmpeg Generator - Converting M3U8 to Command
+      </Typography>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '64px',
+        }}
+      >
+        <input
+          type="text"
+          onChange={handleChange}
+          style={{ width: '306px', height: '28px' }}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            generateAddr(values?.address);
+          }}
+          style={{ width: '96px', height: '48px', marginLeft: '12px' }}
+        >
+          변환
+        </button>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: '32px' }}>
-        <Typography variant="h6" style={{ textAlign: 'center' }}>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 'inherit',
+          marginTop: '32px',
+          overflow: 'hidden',
+        }}
+      >
+        <Typography variant="body2" style={{ textAlign: 'center' }}>
           {addr}
         </Typography>
       </div>
-    </>
+    </div>
   );
 };
 
